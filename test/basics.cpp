a6fc6d21b9ab772667fa0dcec6246e055e860840
Blaise Tine <tinebp@yahoo.com>
2016-10-11 03:38:51 -0400

    + added new tests

--- test/basics.cpp
+++ test/basics.cpp
@@ -17,7 +17,36 @@ TEST_CASE("basic tests", "[basic]") {
       ch_bit4 c = a | 0101_b;
       return (c == 1101_b);
     });
+    TEST([]()->ch_logic {
+      ch_bit4 a(1100_b);
+      ch_bit4 c = a ^ 0101_b;
+      return (c == 1001_b);
+    });
   }  
+  SECTION("test subscript") {
+    TEST([]()->ch_logic {
+      ch_bit4 a(1100_b);
+      ch_bit2 c = a.slice<2>(1) ^ 01_b;
+      return (c == 11_b);
+    });
+    TEST([]()->ch_logic {
+      ch_bit4 a(1100_b);
+      ch_bit2 c = a.aslice<2>(1) ^ 01_b;
+      return (c == 10_b);
+    });
+  }
+  SECTION("test concat") {
+    TEST([]()->ch_logic {
+      ch_bit4 a(1100_b);
+      ch_bitv<5> c = (a, '1') ^ 01010_b;
+      return (c == 10011_b);
+    });
+    TEST([]()->ch_logic {
+      ch_bit4 a(1100_b);
+      ch_bitv<5> c = ('1', a) ^ 00101_b;
+      return (c == 11001_b);
+    });
+  }
 }
 
 
