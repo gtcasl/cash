84102dd1ac5f029047acb3644236487098e94bc0
Blaise Tine <tinebp@yahoo.com>
2016-11-27 17:58:59 -0500

    +minor update

--- opt.cpp
+++ opt.cpp
@@ -51,7 +51,8 @@ size_t optimizer::remove_dead_nodes(const std::set<lnodeimpl*>& live_nodes) {
   auto iter = nodes.begin();
   while (iter != nodes.end()) {
     if (live_nodes.count(*iter) == 0) {
-      DBG(3, "*** deleting node: x%d\n", (*iter)->get_id());
+      lnodeimpl* node = *iter;
+      DBG(3, "*** deleting node: %s%d(#%d)!\n", node->get_name().c_str(), node->get_size(), node->get_id());
       iter = m_ctx->erase_node(iter);
       ++deleted;      
     } else {
