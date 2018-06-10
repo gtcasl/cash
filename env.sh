cd616d3ea4e4027d4beca96bec47f010c7408a68
tinebp <tinebp@yahoo.com>
2018-06-10 17:53:32 -0400

    Delete env.sh

--- env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export CASH_HOME=${PWD}
-export CPATH=${CASH_HOME}/src:${CPATH}
-export LIBRARY_PATH=${CASH_HOME}/src:${LIBRARY_PATH}
-export LD_LIBRARY_PATH=${CASH_HOME}/src:${LD_LIBRARY_PATH}
-export CASH_DEBUG_LEVEL=2
-export CASH_DUMP_AST=1
-export CASH_DEBUG_VERILOG=1
