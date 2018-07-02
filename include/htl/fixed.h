c0be2dca41562953861274995326473d3701d568
tinebp <tinebp@yahoo.com>
2018-07-02 10:28:46 -0400

    backup

--- include/htl/fixed.h
+++ include/htl/fixed.h
@@ -12,9 +12,110 @@ using namespace extension;
 // |S|IIIIIIIIIIIIIIII|FFFFFFFFFFFFFFF|
 
 template <unsigned N, unsigned F>
+class ch_fixed;
+
+template <unsigned N, unsigned F>
+class ch_scfixed : public ch_scbit<N> {
+public:  
+  static_assert(N >= (F+1), "invalid size");
+  using traits = scalar_traits<N, true, ch_scfixed, ch_fixed<N, F>>;
+  using base = ch_scbit<N>;
+  static constexpr unsigned I = N-F;
+
+  explicit ch_scfixed(const scalar_buffer_ptr& buffer = make_scalar_buffer(N))
+    : base(buffer)
+  {}
+
+  template <typename U,
+            CH_REQUIRE_0(std::is_integral_v<U>)>
+  ch_scfixed(const U& other) : base(other) {}
+
+  ch_scfixed(const ch_scbit<N>& other) : base(other) {}
+
+  template <typename U,
+            CH_REQUIRE_0(is_bitvector_extended_type_v<U>)>
+  explicit ch_scfixed(const U& other) : base(other) {}
+
+  explicit ch_scfixed(const ch_bit<N>& other) : base(other) {}
+
+  ch_scfixed(const ch_scfixed& other) : base(other) {}
+
+  ch_scfixed(ch_scfixed&& other) : base(std::move(other)) {}
+
+  ch_scfixed& operator=(const ch_scfixed& other) {
+    base::operator=(other);
+    return *this;
+  }
+
+  ch_scfixed& operator=(ch_scfixed&& other) {
+    base::operator=(std::move(other));
+    return *this;
+  }
+
+  CH_SCALAR_INTERFACE(ch_scfixed)
+
+  friend auto operator==(const ch_scfixed& lhs, const ch_scfixed& rhs) {
+    return (lhs.as_scint() == rhs.as_scint());
+  }
+
+  friend auto operator!=(const ch_scfixed& lhs, const ch_scfixed& rhs) {
+    return (lhs.as_scint() != rhs.as_scint());
+  }
+
+  friend auto operator<(const ch_scfixed& lhs, const ch_scfixed& rhs) {
+    return (lhs.as_scint() < rhs.as_scint());
+  }
+
+  friend auto operator<=(const ch_scfixed& lhs, const ch_scfixed& rhs) {
+    return (lhs.as_scint() <= rhs.as_scint());
+  }
+
+  friend auto operator>(const ch_scfixed& lhs, const ch_scfixed& rhs) {
+    return (lhs.as_scint() >= rhs.as_scint());
+  }
+
+  friend auto operator>=(const ch_scfixed& lhs, const ch_scfixed& rhs) {
+    return (lhs.as_scint() >= rhs.as_scint());
+  }
+
+  friend auto operator-(const ch_scfixed& self) {
+    return (-self.as_scint()).template as<ch_scfixed>();
+  }
+
+  friend auto operator+(const ch_scfixed& lhs, const ch_scfixed& rhs) {
+    return (lhs.as_scint() + rhs.as_scint()).template as<ch_scfixed>();
+  }
+
+  friend auto operator-(const ch_scfixed& lhs, const ch_scfixed& rhs) {
+    return (lhs.as_scint() - rhs.as_scint()).template as<ch_scfixed>();
+  }
+
+  friend auto operator*(const ch_scfixed& lhs, const ch_scfixed& rhs) {
+    auto ret = ((lhs.as_scint().template pad<N+F>() * rhs.as_scint()) >> F).template slice<N>();
+    return ret.template as<ch_scfixed>();
+  }
+
+  friend auto operator/(const ch_scfixed& lhs, const ch_scfixed& rhs) {
+    auto ret = ((lhs.as_scint().template pad<N+F>() << F) / rhs.as_scint()).template slice<N>();
+    return ret.template as<ch_scfixed>();
+  }
+
+  static auto fromInt(const ch_scint<I>& in) {
+    return ch_scfixed((in.template pad<N>() << F));
+  }
+
+  static auto fromReal(double in) {
+    static_assert(N <= 64, "invalid size");
+    auto value = (int64_t)(double(in) * (int64_t(1) << F));
+    return ch_scfixed(value);
+  }
+};
+
+template <unsigned N, unsigned F>
 class ch_fixed : public ch_bit<N> {
-public:
-  using traits = logic_traits<N, true, ch_fixed, ch_scint<N>>;
+public:  
+  static_assert(N >= (F+1), "invalid size");
+  using traits = logic_traits<N, true, ch_fixed, ch_scfixed<N, F>>;
   using base = ch_bit<N>;
   static constexpr unsigned I = N-F;
 
@@ -87,17 +188,23 @@ public:
   }
 
   friend auto operator*(ch_fixed lhs, const ch_fixed& rhs) {
-    auto ret = ch_slice<N>((ch_pad<N+F>(lhs.as_int()) * rhs.as_int()) >> F);
+    auto ret = ((lhs.as_int().template pad<N+F>() * rhs.as_int()) >> F).template slice<N>();
     return ret.template as<ch_fixed>();
   }
 
   friend auto operator/(ch_fixed lhs, const ch_fixed& rhs) {
-    auto ret = ch_slice<N>((ch_pad<N+F>(lhs.as_int()) << F) / rhs.as_int());
+    auto ret = ((lhs.as_int().template pad<N+F>() << F) / rhs.as_int()).template slice<N>();
     return ret.template as<ch_fixed>();
   }
 
   static auto fromInt(const ch_int<I>& in, CH_SLOC) {
     return ch_fixed((in.template pad<N>() << F), sloc);
+  }
+
+  static auto fromReal(double in, CH_SLOC) {
+    static_assert(N <= 64, "invalid size");
+    auto value = (int64_t)(double(in) * (int64_t(1) << F));
+    return ch_fixed(value, sloc);
   }
 };
 
--- tests/fixed.cpp
+++ tests/fixed.cpp
@@ -4,7 +4,7 @@
 using namespace ch::htl;
 
 TEST_CASE("fixed", "[fixed]") {
-  SECTION("arithmetic", "[arithmetic]") {
+  SECTION("logic", "[logic]") {
     TEST([]()->ch_bool {
       ch_fixed<32, 16> a(0x8000), b = ch_fixed<32, 16>::fromInt(1);
       auto c = a + b;
@@ -37,6 +37,64 @@ TEST_CASE("fixed", "[fixed]") {
       auto c = a / b;
       //ch_print("a={0}, b={1}, c={0}", a, b, c);
       return (c == 0x30000);
+    });
+
+    TEST([]()->ch_bool {
+      ch_fixed<32, 16> a = ch_fixed<32, 16>::fromReal(0.25);
+      //ch_print("a={0}", a);
+      return (a == 0x4000);
+    });
+
+    TEST([]()->ch_bool {
+      ch_fixed<32, 16> a = ch_fixed<32, 16>::fromReal(-0.125);
+      //ch_print("a={0}", a);
+      return (a == -0x2000);
+    });
+  }
+
+  SECTION("scalar", "[scalar]") {
+    TESTX([]()->bool {
+      ch_scfixed<32, 16> a(0x8000), b = ch_scfixed<32, 16>::fromInt(1);
+      auto c = a + b;
+      //ch_print("a={0}, b={1}, c={0}", a, b, c);
+      return (c == 0x18000);
+    });
+
+    TESTX([]()->bool {
+      ch_scfixed<32, 16> a(0x18000), b(0x8000);
+      auto c = a - b;
+      return (c == 0x10000);
+    });
+
+    TESTX([]()->bool {
+      ch_scfixed<32, 16> a(0x8000), b(0x8000);
+      auto c = a * b;
+      //ch_print("a={0}, b={1}, c={0}", a, b, c);
+      return (c == 0x4000);
+    });
+
+    TESTX([]()->bool {
+      ch_scfixed<32, 16> a(-0x8000), b(0x8000);
+      auto c = a * b;
+      //ch_print("a={0}, b={1}, c={0}", a, b, c);
+      return (c == -0x4000);
+    });
+
+    TESTX([]()->bool {
+      ch_scfixed<32, 16> a(0x18000), b(0x8000);
+      auto c = a / b;
+      //ch_print("a={0}, b={1}, c={0}", a, b, c);
+      return (c == 0x30000);
+    });
+
+    TESTX([]()->bool {
+      ch_scfixed<32, 16> a = ch_scfixed<32, 16>::fromReal(0.25);
+      return (a == 0x4000);
+    });
+
+    TESTX([]()->bool {
+      ch_scfixed<32, 16> a = ch_scfixed<32, 16>::fromReal(-0.125);
+      return (a == -0x2000);
     });
   }
 }
