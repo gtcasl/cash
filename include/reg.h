8372ef203ce74cac4d9ed9881f8884bec69296ef
tinebp <tinebp@yahoo.com>
2018-07-22 11:10:20 -0400

    backup

--- include/reg.h
+++ include/reg.h
@@ -12,7 +12,7 @@ logic_buffer createRegNode(const lnode& init, const source_location& sloc);
 
 logic_buffer copyRegNode(const lnode& node, const source_location& sloc);
 
-logic_buffer createRegNextNode(const lnode& node);
+logic_buffer getRegNextNode(const lnode& node);
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -21,69 +21,62 @@ class ch_reg_impl final : public T {
 public:  
   using traits = logic_traits<ch_width_v<T>, ch_signed_v<T>, ch_reg<T>, ch_scalar_t<T>>;
   using base = T;
-  
-  struct next_t {
-    next_t(const logic_buffer& buffer) : next(buffer) {}
-    T next;
-  };  
-
-  std::unique_ptr<next_t> __next__;
 
   ch_reg_impl(CH_SLOC)
     : base(createRegNode(ch_width_v<T>, sloc)) {
-    __next__ = std::make_unique<next_t>(createRegNextNode(logic_accessor::data(*this)));
+    __next__ = std::make_unique<next_t>(getRegNextNode(logic_accessor::data(*this)));
   }
 
   template <typename U0,
             CH_REQUIRE_0(std::is_convertible_v<U0, T>)>
   ch_reg_impl(const U0& init0, CH_SLOC)
     : base(createRegNode(logic_accessor::data(T(init0)), sloc)) {
-    __next__ = std::make_unique<next_t>(createRegNextNode(logic_accessor::data(*this)));
+    __next__ = std::make_unique<next_t>(getRegNextNode(logic_accessor::data(*this)));
   }
 
   template <typename U0,
             CH_REQUIRE_0(!std::is_convertible_v<U0, T> && std::is_constructible_v<T, U0>)>
   explicit ch_reg_impl(const U0& init0, CH_SLOC)
     : base(createRegNode(logic_accessor::data(T(init0)), sloc)) {
-    __next__ = std::make_unique<next_t>(createRegNextNode(logic_accessor::data(*this)));
+    __next__ = std::make_unique<next_t>(getRegNextNode(logic_accessor::data(*this)));
   }
 
   template <typename U0, typename U1,
             CH_REQUIRE_0(std::is_constructible_v<T, U0, U1>)>
   explicit ch_reg_impl(const U0& init0, const U1& init1, CH_SLOC)
     : base(createRegNode(logic_accessor::data(T(init0, init1)), sloc)) {
-    __next__ = std::make_unique<next_t>(createRegNextNode(logic_accessor::data(*this)));
+    __next__ = std::make_unique<next_t>(getRegNextNode(logic_accessor::data(*this)));
   }
 
   template <typename U0, typename U1, typename U2,
             CH_REQUIRE_0(std::is_constructible_v<T, U0, U1, U2>)>
   explicit ch_reg_impl(const U0& init0, const U1& init1, const U2& init2, CH_SLOC)
     : base(createRegNode(logic_accessor::data(T(init0, init1, init2)), sloc)) {
-    __next__ = std::make_unique<next_t>(createRegNextNode(logic_accessor::data(*this)));
+    __next__ = std::make_unique<next_t>(getRegNextNode(logic_accessor::data(*this)));
   }
 
   template <typename U0, typename U1, typename U2, typename U3,
             CH_REQUIRE_0(std::is_constructible_v<T, U0, U1, U2, U3>)>
   explicit ch_reg_impl(const U0& init0, const U1& init1, const U2& init2, const U3& init3, CH_SLOC)
     : base(createRegNode(logic_accessor::data(T(init0, init1, init2, init3)), sloc)) {
-    __next__ = std::make_unique<next_t>(createRegNextNode(logic_accessor::data(*this)));
+    __next__ = std::make_unique<next_t>(getRegNextNode(logic_accessor::data(*this)));
   }
 
   template <typename U0, typename U1, typename U2, typename U3, typename... Us,
             CH_REQUIRE_0(std::is_constructible_v<T, U0, U1, U2, U3, Us...>)>
   explicit ch_reg_impl(const U0& init0, const U1& init1, const U2& init2, const U3& init3, const Us&... inits)
     : base(createRegNode(logic_accessor::data(T(init0, init1, init2, init3, inits...)), source_location())) {
-    __next__ = std::make_unique<next_t>(createRegNextNode(logic_accessor::data(*this)));
+    __next__ = std::make_unique<next_t>(getRegNextNode(logic_accessor::data(*this)));
   }
 
   ch_reg_impl(const ch_reg_impl& other, CH_SLOC)
     : base(copyRegNode(logic_accessor::data(other), sloc)) {
-    __next__ = std::make_unique<next_t>(createRegNextNode(logic_accessor::data(*this)));
+    __next__ = std::make_unique<next_t>(getRegNextNode(logic_accessor::data(*this)));
   }
 
   ch_reg_impl(ch_reg_impl&& other)
     : base(std::move(other.buffer_)) {
-    __next__ = std::make_unique<next_t>(createRegNextNode(logic_accessor::data(*this)));
+    __next__ = std::make_unique<next_t>(getRegNextNode(logic_accessor::data(*this)));
   }
 
   auto& operator->() const {
@@ -94,6 +87,12 @@ private:
 
   ch_reg_impl& operator=(ch_reg_impl&&) = delete;
 
+  struct next_t {
+    next_t(const logic_buffer& buffer) : next(buffer) {}
+    T next;
+  };
+
+  std::unique_ptr<next_t> __next__;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
--- src/regimpl.cpp
+++ src/regimpl.cpp
@@ -7,45 +7,6 @@
 
 using namespace ch::internal;
 
-logic_buffer ch::internal::createRegNode(unsigned size, const source_location& sloc) {
-  auto ctx = ctx_curr();
-  auto reg = ctx->create_node<regimpl>(
-        ctx->create_node<proxyimpl>(size, sloc), sloc);
-  logic_buffer ret(reg, sloc);
-  reg->next().write(0, ret.data(), 0, size, sloc);
-  return ret;
-}
-
-logic_buffer ch::internal::createRegNode(const lnode& init, const source_location& sloc) {
-  auto ctx = init.impl()->ctx();
-  auto reg = ctx->create_node<regimpl>(
-        ctx->create_node<proxyimpl>(init.size(), sloc), init, sloc);
-  logic_buffer ret(reg, sloc);
-  reg->next().write(0, ret.data(), 0, init.size(), sloc);
-  return ret;
-}
-
-logic_buffer ch::internal::copyRegNode(const lnode& node, const source_location& sloc) {
-  auto proxy = reinterpret_cast<proxyimpl*>(node.impl());
-  auto reg = reinterpret_cast<regimpl*>(proxy->src(0).impl());
-  auto ctx = proxy->ctx();
-  regimpl* new_reg;
-  if (reg->has_init()) {
-    new_reg = ctx->create_node<regimpl>(reg->next(), reg->init(), sloc);
-  } else {
-    new_reg = ctx->create_node<regimpl>(reg->next(), sloc);
-  }
-  return logic_buffer(new_reg, sloc);
-}
-
-logic_buffer ch::internal::createRegNextNode(const lnode& node) {
-  auto proxy = reinterpret_cast<proxyimpl*>(node.impl());
-  auto reg = reinterpret_cast<regimpl*>(proxy->src(0).impl());
-  return logic_buffer(lnode(proxy->size(), reg->next(), 0, proxy->sloc()));
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
 regimpl::regimpl(context* ctx, const lnode& next, const source_location& sloc)
   : lnodeimpl(ctx, type_reg, next.size(), sloc) {
   auto cd = ctx->current_cd(sloc);
@@ -124,4 +85,43 @@ ch_bit<1> ch::internal::ch_clock(const source_location& sloc) {
 ch_bit<1> ch::internal::ch_reset(const source_location& sloc) {
   auto reset = ctx_curr()->current_reset(sloc);
   return make_type<ch_bit<1>>(reset, sloc);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+logic_buffer ch::internal::createRegNode(unsigned size, const source_location& sloc) {
+  auto ctx = ctx_curr();
+  auto reg = ctx->create_node<regimpl>(
+        ctx->create_node<proxyimpl>(size, sloc), sloc);
+  logic_buffer ret(reg, sloc);
+  reg->next().write(0, ret.data(), 0, size, sloc);
+  return ret;
+}
+
+logic_buffer ch::internal::createRegNode(const lnode& init, const source_location& sloc) {
+  auto ctx = init.impl()->ctx();
+  auto reg = ctx->create_node<regimpl>(
+        ctx->create_node<proxyimpl>(init.size(), sloc), init, sloc);
+  logic_buffer ret(reg, sloc);
+  reg->next().write(0, ret.data(), 0, init.size(), sloc);
+  return ret;
+}
+
+logic_buffer ch::internal::copyRegNode(const lnode& node, const source_location& sloc) {
+  auto proxy = reinterpret_cast<proxyimpl*>(node.impl());
+  auto reg = reinterpret_cast<regimpl*>(proxy->src(0).impl());
+  auto ctx = proxy->ctx();
+  regimpl* new_reg;
+  if (reg->has_init()) {
+    new_reg = ctx->create_node<regimpl>(reg->next(), reg->init(), sloc);
+  } else {
+    new_reg = ctx->create_node<regimpl>(reg->next(), sloc);
+  }
+  return logic_buffer(new_reg, sloc);
+}
+
+logic_buffer ch::internal::getRegNextNode(const lnode& node) {
+  auto proxy = reinterpret_cast<proxyimpl*>(node.impl());
+  auto reg = reinterpret_cast<regimpl*>(proxy->src(0).impl());
+  return logic_buffer(lnode(proxy->size(), reg->next(), 0, proxy->sloc()));
 }
