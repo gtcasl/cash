a28ab735405722d004b0cf379917fb84c3cb205b
Blaise Tine <tinebp@yahoo.com>
2019-09-04 16:13:51 -0400

    update

--- src/platform.h
+++ src/platform.h
@@ -6,27 +6,27 @@ namespace ch {
 namespace internal {
 
 enum class cflags {
-  debug_cfg       = (1 << 0),
-  dump_cfg        = (1 << 1),
-  dump_ast        = (1 << 2),
-  dump_asm        = (1 << 3),
-  codegen_sloc    = (1 << 4),
-  codegen_merged  = (1 << 5),
-  check_reg_init  = (1 << 6),
-  force_reg_init  = (1 << 7),
-  disable_jit     = (1 << 8),
-  disable_pip     = (1 << 9),
-  disable_cfo     = (1 << 10),
-  disable_cse     = (1 << 11),
-  disable_bro     = (1 << 12),
-  disable_rpo     = (1 << 13),
-  disable_pcx     = (1 << 14),
-  disable_swo     = (1 << 15),
-  disable_sro     = (1 << 16),
-  disable_snc     = (1 << 17),
-  disable_cpb     = (1 << 18),
-  disable_smo     = (1 << 19),
-  verbose_tracing = (1 << 20)
+  debug_cfg       = (1 << 0),  // 1
+  dump_cfg        = (1 << 1),  // 2
+  dump_ast        = (1 << 2),  // 4
+  dump_asm        = (1 << 3),  // 8
+  codegen_sloc    = (1 << 4),  // 16
+  codegen_merged  = (1 << 5),  // 32
+  check_reg_init  = (1 << 6),  // 64
+  force_reg_init  = (1 << 7),  // 128
+  disable_jit     = (1 << 8),  // 256
+  disable_pip     = (1 << 9),  // 512
+  disable_cfo     = (1 << 10), // 1024
+  disable_cse     = (1 << 11), // 2048
+  disable_bro     = (1 << 12), // 4096
+  disable_rpo     = (1 << 13), // 8192
+  disable_pcx     = (1 << 14), // 16384
+  disable_swo     = (1 << 15), // 32768
+  disable_sro     = (1 << 16), // 65536
+  disable_snc     = (1 << 17), // 131072
+  disable_cpb     = (1 << 18), // 262144
+  disable_smo     = (1 << 19), // 524288
+  verbose_tracing = (1 << 20)  // 1048576
 };
 
 inline constexpr auto operator|(cflags lsh, cflags rhs) {
